8d0cbc794e9ab1516a2906c1c793166f1c8fc4e0
Kavali7 <georgesbusiness54@gmail.com>
2025-09-24 17:55:01 +0100

    Corrige la sélection active de la top bar

--- src/navigation/PatchedMaterialTopTabBar.tsx
+++ src/navigation/PatchedMaterialTopTabBar.tsx
@@ -7,14 +7,21 @@ const PatchedMaterialTopTabBar: React.FC<MaterialTopTabBarProps> = props => {
 
   const renderTabBarItem = useCallback(
     (itemProps: React.ComponentProps<typeof TabBarItem> & { key: string }) => {
-      const { key, ...tabBarItemProps } = itemProps;
+      const { key: providedKey, route, ...tabBarItemProps } = itemProps;
+      const elementKey = route?.key ?? providedKey;
+      const baseProps = { ...tabBarItemProps, route };
 
       if (originalRender) {
-        const element = originalRender({ ...tabBarItemProps, key } as typeof itemProps);
-        return React.isValidElement(element) ? React.cloneElement(element, { key }) : element;
+        const element = originalRender({ ...(baseProps as typeof itemProps), key: elementKey });
+        return React.isValidElement(element) ? React.cloneElement(element, { key: elementKey }) : element;
       }
 
-      return <TabBarItem key={key} {...(tabBarItemProps as React.ComponentProps<typeof TabBarItem>)} />;
+      return (
+        <TabBarItem
+          key={elementKey}
+          {...(baseProps as React.ComponentProps<typeof TabBarItem>)}
+        />
+      );
     },
     [originalRender]
   );
