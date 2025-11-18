0a8156a63ca112109fa86f405ba8fd8b78578de7
Kavali7 <georgesbusiness54@.comm>
2025-11-18 17:12:23 +0100

    fix: normalize FormDétails package metadata

--- FormDétails/next-env.d.ts
+++ FormDétails/next-env.d.ts
@@ -1,9 +1,5 @@
-﻿/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/navigation-types/compat/navigation" />
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
 
-declare module '*.svg' {
-  import type { ReactElement, SVGProps } from 'react';
-  const content: ReactElement<SVGProps<SVGSVGElement>>;
-  export default content;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
