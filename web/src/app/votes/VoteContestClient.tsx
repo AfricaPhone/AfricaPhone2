ab1ec5f4418f3836850509268c479db6fadd9774
Kavali7 <georgesbusiness54@gmail.com>
2025-12-06 20:43:08 +0100

    Update votes page download button text to include AfricaPhone

--- web/src/app/votes/VoteContestClient.tsx
+++ web/src/app/votes/VoteContestClient.tsx
@@ -733,7 +733,7 @@ export default function VoteContestClientPage() {
             <svg viewBox="0 0 24 24" className="h-4 w-4" fill="currentColor">
               <path d="M3.609 1.814L13.792 12 3.61 22.186a.996.996 0 0 1-.61-.92V2.734a1 1 0 0 1 .609-.92zm10.89 10.893l2.302 2.302-10.937 6.333 8.635-8.635zm3.199-3.198l2.807 1.626a1 1 0 0 1 0 1.73l-2.808 1.626L15.206 12l2.492-2.491zM5.864 2.658L16.8 8.99l-2.302 2.302-8.634-8.634z" />
             </svg>
-            Télécharger l&apos;application
+            Télécharger l&apos;application AfricaPhone
             <svg className="h-4 w-4 text-white/70 transition-transform group-hover:translate-x-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
               <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
             </svg>
